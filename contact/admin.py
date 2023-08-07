161dfccfefe310ef34ac693da03919cdcab1edf5
Lobo_RJ <lobo.prlc@gmail.com>
2023-08-07 12:57:57 -0300

    add contact/views dir

--- contact/admin.py
+++ contact/admin.py
@@ -6,13 +6,15 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     # campos que serão apresentados na lista
     list_display = ['id', 'first_name', 'last_name', 'phone',
-                    'email', 'created_date', 'modified_date']
+                    'email', 'show', 'created_date', 'modified_date']
     # campos usados para ordenação. O sinal '-' indica a ordem decrescente
     ordering = ['-id']
     # campos utilizados para filtro
     list_filter = ['created_date', 'category', 'owner']
     # campos de busca
     search_fields = ['first_name', 'last_name', 'email']
+    # campos editáveis na lista
+    list_editable = ['show']
     # quantidade de registros mostrados por página
     list_per_page = 20
     # quantidade de registros mostrados ao selecionar 'Mostrar tudo'
--- contact/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(
-        request,
-        template_name='contact/index.html',
-        context={'title': 'Agenda'}
-    )
